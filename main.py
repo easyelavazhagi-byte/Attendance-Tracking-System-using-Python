cbaaf5b3fa2c4d393cb0c775c623ab60246a49c3
easyelavazhagi-byte <easyelavazhagi@gmail.com>
2026-04-01 16:30:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+OUTPUT:
+
+------ Attendance Tracking System ------
+1. Add Student
+2. Mark Attendance
+3. View Attendance
+4. Calculate Percentage
+5. Exit
+Enter your choice: 1
+Enter student name: Arun
+Student added successfully!
+
+------ Attendance Tracking System ------
+1. Add Student
+2. Mark Attendance
+3. View Attendance
+4. Calculate Percentage
+5. Exit
+Enter your choice: 2
+Enter student name: Arun
+Enter attendance (P/A): P
+Attendance marked!
+
+------ Attendance Tracking System ------
+1. Add Student
+2. Mark Attendance
+3. View Attendance
+4. Calculate Percentage
+5. Exit
+Enter your choice: 4
+Enter student name: Arun
+Arun's attendance percentage: 100.00%
